266e267baa1be3f90abafe158d0f9bcc2762da97
pomarulit007-cyber <poma.rulit007@gmail.com>
2026-02-02 04:21:52 +0300

    Add abstract class

--- src/Test.kt
+++ src/Test.kt
@@ -15,3 +15,41 @@ fun main() {
     val (id, name, quantity) = item1
     println("Декомпозиция: id=$id, name=$name, quantity=$quantity")
 }
+
+abstract class Human(val name: String) {
+    abstract fun hello()
+    abstract var age: Int
+}
+
+class Person(name: String, override var age: Int) : Human(name) {
+    override fun hello() {
+        println("My name is $name")
+    }
+}
+
+abstract class Figure {
+    abstract fun perimeter(): Float
+    abstract fun area(): Float
+}
+
+class Rectangle(val width: Float, val height: Float) : Figure() {
+    override fun perimeter(): Float {
+        return 2 * (width + height)
+    }
+
+    override fun area(): Float {
+        return width * height
+    }
+}
+
+fun main() {
+
+    // val human = Human("Иван")
+
+    val person = Person("Иван", 25)
+    person.hello()
+
+    val rectangle = Rectangle(5f, 3f)
+    println("Периметр прямоугольника: ${rectangle.perimeter()}")
+    println("Площадь прямоугольника: ${rectangle.area()}")
+}
